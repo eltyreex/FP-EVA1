8d1100a884f2a13e50336ff42b28951b637fd045
eltyreex <jofer.dipperfalls@gmail.com>
2025-08-26 21:18:41 -0600

    Add files via upload

---
+++ Eva1_2_datos/src/eva1_p2_datos/Eva1_p2_datos.java
@@ -0,0 +1,20 @@
+
+package eva1_p2_datos;
+
+public class Eva1_p2_datos {
+
+    public static void main(String[] args) {
+        // si usamos el sout y luego tab se ahorra el progreso
+        // ;-- fin de la instruccion
+        //comentarios de linea
+        System.out.println("jofer");//cadena de texto
+        System.out.println(1000);// numero entero
+        System.out.println(34.90);//numero decimal
+        System.out.println(true);//valores logicos verdadero o falso
+        System.out.println(false);
+        System.out.println('A');//caracter
+        System.out.println("A");//cadena de texto
+        
+    }
+    
+}
