b6b74c6d6366e99b8663259b0c47c18f06129995
eltyreex <jofer.dipperfalls@gmail.com>
2025-08-26 21:18:07 -0600

    Add files via upload

---
+++ EVA1_1_Hola_Mundo/src/eva1_hola_mundo/EVA1_Hola_Mundo.java
@@ -0,0 +1,11 @@
+package eva1_hola_mundo;
+
+public class EVA1_Hola_Mundo {
+    
+    public static void main(String[] args) {
+         System.out.println("hola Mundo ");
+         System.out.println("Fundamentos de programacion");
+         
+    }
+    
+}
