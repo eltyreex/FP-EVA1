4afce4a259651a9b301d0cbbea423e4c724f99f6
eltyreex <jofer.dipperfalls@gmail.com>
2025-08-26 21:21:04 -0600

    Add files via upload

---
+++ Eva1_3_print/src/eva1_3_print/Eva1_3_print.java
@@ -0,0 +1,17 @@
+package eva1_3_print;
+
+public class Eva1_3_print {
+
+     public static void main(String[] args) {
+         System.out.print("Nombre del estudiante:");
+         System.out.println("Joaquin Fernando");
+         System.out.print("Numero de control:");
+         System.out.println("25550293");
+         System.out.print("Edad:");
+         System.out.println(18);
+         
+         
+                                    
+    }
+    
+}
---
+++ Eva1_4_empleo/src/eva1_4_empleo/Eva1_4_empleo.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_4_empleo;
+
+/**
+ *
+ * @author fer
+ */
+public class Eva1_4_empleo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("empleo: ");
+        System.out.println("Bombero a domicilio");
+        System.out.print("salario: ");
+        System.out.println("$20");
+        System.out.print("horas a la semana:");
+        System.out.println(1);
+        System.out.print("requiere ingles? ");
+        System.out.println(false);
+    }
+    
+}
