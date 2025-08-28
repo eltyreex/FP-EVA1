ac790451257288a8a9f4914d4939740f91224f06
eltyreex <jofer.dipperfalls@gmail.com>
2025-08-28 14:18:48 -0600

    Add files via upload

---
+++ EVA1_5_Variables/src/eva1_5_variables/EVA1_5_Variables.java
@@ -0,0 +1,20 @@
+package eva1_5_variables;
+public class EVA1_5_Variables {
+    public static void main(String[] args) {
+        //declaracion de variables
+        //inicializacion
+        //uso de variables
+        //crearlas las variables para que se pueden ussar
+        //va primero el tipo de dato luego el identificador
+        String nombre;
+        byte edad; // pa guardar info de edad es bueno usar lso enteros ya que andie tiene 89.3 y es bueno el byte ya q es suficiente 
+        //usamo el operador = -- eso es operador de asignacion
+        nombre = "Pedro miguel"; //pon "Pedro miguel" dentro de la variable nombre
+        edad = 18; //pon "18" en variable edad
+        //no se puede mezclar datos en java si es string es eso y si no es byte no se mezcla
+        // no podemos pasarnos de la informacion q se le asigno si es byte es de 127 no se pase de ahi
+        System.out.println(nombre);
+        System.out.println(edad);
+    }
+    
+}
