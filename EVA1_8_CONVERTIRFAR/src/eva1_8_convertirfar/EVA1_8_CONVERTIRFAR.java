c9189fbb7e9383aa33d3e6fdfd5ee40bba68f8f2
eltyreex <jofer.dipperfalls@gmail.com>
2025-09-01 17:42:00 -0600

    Add files via upload

--- EVA1_8_CONVERTIRFAR/src/eva1_8_convertirfar/EVA1_8_CONVERTIRFAR.java
+++ EVA1_8_CONVERTIRFAR/src/eva1_8_convertirfar/EVA1_8_CONVERTIRFAR.java
@@ -4,13 +4,16 @@ package eva1_8_convertirfar;
 public class EVA1_8_CONVERTIRFAR {
 
     public static void main(String[] args) {
-        double cel, far;
-        cel = 86; //pues en esto es lo mismo que en todos lados en lo cual es primero defino la variable con double despues
+        double cel, far, kel;
+        cel = 35; //pues en esto es lo mismo que en todos lados en lo cual es primero defino la variable con double despues
         // hago lo que siempre me queda mejor lo cual es organizar segun los parametros 
         //c* 1.8 mas 32
-        far =(cel * 1.8) + 32; // aqui lo que no hice fue poner la varible la puse de forma como numero osea que pues no hice uso de la variable cel
+        far = ((9*cel)/5)+32; // aqui lo que no hice fue poner la varible la puse de forma como numero osea que pues no hice uso de la variable cel
         System.out.println(far);
-       
-    }
+        kel = (cel + 273.15);
+        System.out.println(kel);
+        cel = (5*(far - 32)) /9; //me falto el mugre *
+        System.out.println(cel);
+           }
     
 }
